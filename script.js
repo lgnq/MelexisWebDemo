7c9e4c508eaed274f09d10f814a0c0d1e4c12b31
lgnq <dzzxzz@gmail.com>
2024-12-19 11:16:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -469,18 +469,47 @@ var meter = function(p)
 
   p.draw = function() 
   {
-    p.background(255);
-    // p.setCenter(p.width / 2, p.height);
-    p.stroke("#000");
-    p.strokeWeight(2);
-    p.fill(230);
-    p.polarEllipse(0, 190, 190, 0);
-    p.polarLines(12, 10, 180);
-    p.polarLines(60, 5, 180);
-    p.polarLine( 90, 180, 10)
-    p.rotate(p.map(slider.value(), 0, 100, 0, PI));
-    p.strokeWeight(2);
-    p.line(0, 0, -175, 0);
+    p.background(0);
+    p.translate(200, 200);
+    p.rotate(-90);
+  
+    // let hr = hour();
+    // let mn = minute();
+    let sc = p.second();
+  
+    p.strokeWeight(8);
+    p.stroke(255, 100, 150);
+    p.noFill();
+    let secondAngle = p.map(sc, 0, 60, 0, 360);
+    //arc(0, 0, 300, 300, 0, secondAngle);
+  
+    // p.stroke(150, 100, 255);
+    // let minuteAngle = p.map(mn, 0, 60, 0, 360);
+    // //arc(0, 0, 280, 280, 0, minuteAngle);
+  
+    // p.stroke(150, 255, 100);
+    // let hourAngle = p.map(hr % 12, 0, 12, 0, 360);
+    // //arc(0, 0, 260, 260, 0, hourAngle);
+  
+    p.push();
+    p.rotate(secondAngle);
+    p.stroke(255, 100, 150);
+    p.line(0, 0, 100, 0);
+    p.pop();
+  
+    // p.push();
+    // p.rotate(minuteAngle);
+    // p.stroke(150, 100, 255);
+    // p.line(0, 0, 75, 0);
+    // p.pop();
+  
+    // p.push();
+    // p.rotate(hourAngle);
+    // p.stroke(150, 255, 100);
+    // p.line(0, 0, 50, 0);
+    // p.pop();
+  
+    p.stroke(255);
   }
 
   p.windowResized = function() 
