d269adeb7e1236ddaff753edfc35d2ee53f2f451
lgnq <dzzxzz@gmail.com>
2024-12-19 11:40:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -481,15 +481,15 @@ var meter = function(p)
     p.stroke(255, 100, 150);
     p.noFill();
     let secondAngle = p.map(sc, 0, 60, 0, 360);
-    arc(0, 0, 300, 300, 0, secondAngle);
+    p.arc(0, 0, 300, 300, 0, secondAngle);
   
     p.stroke(150, 100, 255);
     let minuteAngle = p.map(mn, 0, 60, 0, 360);
-    arc(0, 0, 280, 280, 0, minuteAngle);
+    p.arc(0, 0, 280, 280, 0, minuteAngle);
   
     p.stroke(150, 255, 100);
     let hourAngle = p.map(hr % 12, 0, 12, 0, 360);
-    arc(0, 0, 260, 260, 0, hourAngle);
+    p.arc(0, 0, 260, 260, 0, hourAngle);
   
     p.push();
     p.rotate(secondAngle);
