f7e50bde62cad9a1f416c2308a05fb6cd3ea4b1d
lgnq <dzzxzz@gmail.com>
2024-12-22 20:49:42 +0800

    Update script_mlx90382.js

--- script_mlx90382.js
+++ script_mlx90382.js
@@ -497,14 +497,16 @@ var meter = function(p)
     }
     p.pop();
   
+    p.push();
     p.translate(0, 0);
     p.rotate(90);
     p.fill(255, 100, 150);
-    p.text("angle is " + x, 50, 50, 50, 200);
+    p.text("angle is " + x, 50, 50, 100, 200);
     p.fill('limegreen');
-    p.text("temperature is " + t + " °", 50, 100, 50, 200);
+    p.text("temperature is " + t + " °", 50, 100, 100, 200);
+    p.pop();
 
-    p.stroke(255);
+    // p.stroke(255);
   }
 
   p.windowResized = function() 
