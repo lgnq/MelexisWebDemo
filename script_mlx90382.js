22d551616b8bd13067d2be5c06ddac48a96c34f0
lgnq <dzzxzz@gmail.com>
2024-12-22 20:38:42 +0800

    Update script_mlx90382.js

--- script_mlx90382.js
+++ script_mlx90382.js
@@ -499,8 +499,9 @@ var meter = function(p)
   
     p.translate(0, 0);
     p.rotate(90);
-    p.fill('limegreen');
+    p.fill(255, 100, 150);
     p.text("angle is " + x, 50, 50, 50, 200);
+    p.fill('limegreen');
     p.text("temperature is " + t + " °", 50, 100, 50, 200);
 
     p.stroke(255);
